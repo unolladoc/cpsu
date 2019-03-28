9e6fd3ea0338e791c17351ea77ae576135dba171
Uno Lladoc <rubenlladoc@gmail.com>
2019-03-28 14:34:09 +0800

    updated database

--- cpsu.sql
+++ cpsu.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Server version:               10.1.37-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win32
--- HeidiSQL Version:             9.5.0.5196
+-- HeidiSQL Version:             10.1.0.5464
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `files` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table cpsu.files: ~0 rows (approximately)
+-- Dumping data for table cpsu.files: ~1 rows (approximately)
 /*!40000 ALTER TABLE `files` DISABLE KEYS */;
 /*!40000 ALTER TABLE `files` ENABLE KEYS */;
 
@@ -38,14 +38,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `type` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Dumping data for table cpsu.user: ~1 rows (approximately)
+-- Dumping data for table cpsu.user: ~4 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `username`, `password`, `name`) VALUES
-	(0000000001, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator');
+INSERT INTO `user` (`id`, `username`, `password`, `name`, `type`) VALUES
+	(0000000001, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator', 1),
+	(0000000002, 'xiari', '21232f297a57a5a743894a0e4a801fc3', 'Xiari Ann Iligan', 2),
+	(0000000004, 'uno', 'f13d6cafac96af5134839c0385d051bc', 'Ruben Lladoc', 0),
+	(0000000006, 'jam', '21232f297a57a5a743894a0e4a801fc3', 'Jose Maningo', 1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
