c7af50e7be9e57c377bbe5e89e47a4b02c715232
unolladoc <rubenlladoc@gmail.com>
2019-04-24 09:14:15 +0800

    working php json encode

--- loaddata.php
+++ loaddata.php
@@ -1,16 +1,27 @@
 <?php 
 include('conn.php');
 
-if(isset($_POST['cid'])) {
-   // $sql = "SELECT * from offices where campus=" . $_POST['cid'];
-   // $result = $conn->query($sql);
-   $stmt = $conn->prepare("SELECT * from offices where campus=" . $_POST['cid']);
-   $stmt->execute();
-   $result = $stmt->get_result();
-   $row = $result->fetch_assoc(MYSQLI_ASSOC);
-   echo json_encode($row);
+header("Content-Type: application/json; charset=UTF-8");
+$obj = json_decode($_POST["x"], false);
 
-   
-}
+//$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+$conn = new mysqli($servername, $username, $password, $dbname);
+$stmt = $conn->prepare("SELECT * from offices where campus = ?");
+$stmt->bind_param("s", $obj->campus);
+$stmt->execute();
+$result = $stmt->get_result();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($outp);
+
+// if(isset($_POST['cid'])) {
+//    // $sql = "SELECT * from offices where campus=" . $_POST['cid'];
+//    // $result = $conn->query($sql);
+//    $stmt = $conn->prepare("SELECT * from offices where campus=" . $_POST['cid']);
+//    $stmt->execute();
+//    $result = $stmt->get_result();
+//    $row = $result->fetch_assoc();
+//    echo json_encode($row);  
+// }
 
 ?>
